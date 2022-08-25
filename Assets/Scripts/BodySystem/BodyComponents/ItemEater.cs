13975156c24914ef44134a0190ee5e7e1274a823
Ben Buckton <contact@benbuckton.com>
2022-08-25 21:55:32 +0200

    Item Eater

---
+++ Assets/Scripts/BodySystem/BodyComponents/ItemEater.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ItemEater: BodyComponent, IWorldUpdateMember {
+    public float respawnTime = 5f;
+    
+    private float _nextUpdate;
+    private ItemInventory _itemInventory;
+    
+    public override void Init() {
+        base.Init();
+        _itemInventory = GetComponent<ItemInventory>();
+        _nextUpdate = Time.time + respawnTime;
+        SceneAccess.instance.worldSystem.RegisterMember(this);
+    }
+
+    public void WorldUpdate() {
+        if (Time.time >= _nextUpdate) {
+            if (_itemInventory.HasItemToTake()) {
+                _itemInventory.TakeItem();
+            }
+            _nextUpdate += respawnTime;
+        }
+    }
+}
